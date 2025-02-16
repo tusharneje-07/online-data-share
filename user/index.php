47550636795e1179c298ac219423e9a22d601bfc
Tushar <nejetushar07@gmail.com>
2025-02-16 23:19:41 +0530

    Profile Page Update => 2 & User Query

--- user/index.php
+++ user/index.php
@@ -1,6 +1,27 @@
 <?php
 session_start();
+require_once './db.php';
 
-header("Location: login");
-exit();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    header("Location: login");
+} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if(!isset($_GET['q'])){
+        header("Location: login");
+        exit();
+    }
+
+    $uid = $_GET['q'];
+    echo $uid;
+
+    $stmt = $pdo->prepare("SELECT * FROM user_information WHERE uid = ?");
+    $stmt->execute([$uid]);
+    var_dump($stmt->fetch());
+
+
+    
+} else {
+    echo "This is a " . $_SERVER['REQUEST_METHOD'] . " request.";
+}
+
 ?>
